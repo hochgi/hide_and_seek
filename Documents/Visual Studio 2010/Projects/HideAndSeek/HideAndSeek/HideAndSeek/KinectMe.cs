e9c8ba4f078d83a86f667b2409732115cc0b6fdd
tamar <tamarsamson@gmail.com>
2012-08-06 16:38:22 +0300

    implementing algorithm

---
+++ Documents/Visual Studio 2010/Projects/HideAndSeek/HideAndSeek/HideAndSeek/KinectMe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HideAndSeek
+{
+    class KinectMe
+    {
+        internal bool isWalking()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool isWalkingRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool isWalkingLeft()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
