3c3e6ad29f5cd9ebe8c29ef35fcc6429e5bff1a0
Gal <galgil88@gmail.com>
2012-09-11 20:46:47 +0300

    added ideas and partial functinality to the kinect handler

--- HideAndSeek/HideAndSeek/KinectMe.cs
+++ HideAndSeek/HideAndSeek/KinectMe.cs
@@ -7,19 +7,61 @@ namespace HideAndSeek
 {
     class KinectMe
     {
+        Queue<FeetState> walkHistory;
+
+        internal KinectMe()
+        {
+            walkHistory = new Queue<FeetState>(20);
+            for (int i = 0; i < 20; i++) 
+            {
+                walkHistory.Enqueue(new FeetState());
+            }
+        }
+
+        //what about facing direction?? speed??
         internal bool isWalking()
         {
             throw new NotImplementedException();
+            // TODO: check 4 last FeetState instances for a walk cycle.
         }
 
+        //note that this function returns the position of the head including Z-coordinate
+        //this can be used to conbinate the virtual position with the actual position 
+        internal Microsoft.Xna.Framework.Vector3 getHeadPosition()
+        {
+            throw new NotImplementedException();
+            // TODO: get the head position from skeleton-detecion
+        }
+
+        
+        /* this functions will be omitted! do not use them!!
+         * 
         internal bool isWalkingRight()
         {
             throw new NotImplementedException();
         }
-
+        
         internal bool isWalkingLeft()
         {
             throw new NotImplementedException();
+        }
+        */
+
+        /*
+         * class represents the feet-state 
+         */
+        private class FeetState // stink!! ;)
+        {
+            public enum footPosition { Up, Down };
+            footPosition left, right;
+            DateTime timeStamp;
+
+            public FeetState() 
+            {
+                left = footPosition.Down;
+                right = footPosition.Down;
+                timeStamp = DateTime.Now;
+            }
         }
     }
 }
