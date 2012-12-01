0fd91332dad81825288ea34a90e3c87533199ac2
hochgi <giladhoch@yahoo.com>
2012-12-01 19:29:08 +0200

    started working on the orthogonal to camera sprites system

---
+++ HideAndSeek/HideAndSeek/Billboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace HideAndSeek
+{
+    interface Billboard
+    {
+        Vector3[] getQuadBillboard(Vector3 up, Vector3 side);
+
+        Vector3 getPosition();
+
+        static Billboard[] sortBillboards(Billboard[] billboards, Vector3 cameraLocation)
+        {
+            Array.Sort<Billboard>(billboards, new Comparison<Billboard>(
+                (Billboard a, Billboard b) => (int)(Vector3.DistanceSquared(cameraLocation,a.getPosition()) - Vector3.DistanceSquared(cameraLocation,b.getPosition())))
+            );
+            throw new NotImplementedException;
+        }
+
+        //static void drawBillboards
+    }
+}
