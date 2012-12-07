987a9250a596e90fa4ac672237929403b6db0064
hochgi <giladhoch@yahoo.com>
2012-12-07 13:07:50 +0200

    finished billboards systems. started implementing that on trees

---
+++ HideAndSeek/HideAndSeek/AlreadyInitializedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HideAndSeek
+{
+    class AlreadyInitializedException : Exception
+    {
+        public AlreadyInitializedException(string p) : base(p) {}
+    }
+}
